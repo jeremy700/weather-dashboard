871e31d7b3c0b4f11d8fd4b40cb37f6a19dddff0
Jeremias Arias <jeremyarias70@gmail.com>
2026-02-03 20:08:15 -0500

    instalación de bootstrap, arquitectura inicial y configuracion inicial de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Forecast } from './components/forecast/forecast';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'dashboard', component: Dashboard},
+    {path: 'forecast', component: Forecast},
+    {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+    {path: '**', redirectTo: 'dashboard'}
+];
---
+++ src/app/components/forecast/forecast.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forecast',
+  standalone: true,
+  imports: [],
+  templateUrl: './forecast.html',
+  styleUrl: './forecast.css',
+})
+export class Forecast {
+
+}
